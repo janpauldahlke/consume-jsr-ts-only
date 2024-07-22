969813c39155a50e301532ef3ffc2d9d0082bb16
Jan Dahlke <jan.dahlke@akquinet.de>
2024-07-22 15:48:59 +0200

    questions in readme

---
+++ main.ts
@@ -0,0 +1,8 @@
+import { isOdd } from "@hail-discordia/is-odd-ts";
+
+export function main() {
+
+  console.log(isOdd(2))
+}
+
+main()
---
+++ readme.md
@@ -0,0 +1,3 @@
+why does `deno run main.ts` work
+
+and the chain of `tsc main.ts` --> `node main.js` doesnt
