7457237a80289b6eee7d6ea27d3b1303a7aac32e
Jan Dahlke <jan.dahlke@akquinet.de>
2024-07-22 16:12:44 +0200

    provides tsconfig and nodenext module resolution to work in node

---
+++ dist/main.js
@@ -0,0 +1,5 @@
+import { isOdd } from "@hail-discordia/is-odd-ts";
+export function main() {
+    console.log(isOdd(2));
+}
+main();
--- main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var is_odd_ts_1 = require("@hail-discordia/is-odd-ts");
-function main() {
-    console.log((0, is_odd_ts_1.isOdd)(2));
-}
-main();
